8d60924727219e4bd09f33f3652637c8c66fce96
Jason Viger <jason.viger@gmail.com>
2018-08-20 08:09:19 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,7 +10,7 @@ class Song
   end
   
   def artist_name(name)
-    self.artist = Artist.find
+    self.artist = Artist.find_or_create_by_name(name)
   end
   
   def self.new_by_filename(filename)
