89f2ad491ca7dba0a4ae57798fe60c4e714b6b38
Jason Viger <jason.viger@gmail.com>
2018-08-20 08:17:23 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -17,8 +17,8 @@ class Song
   def self.new_by_filename(filename)
     song_split = filename.split(" - ")
     song = Song.new(song_split[1])
+    song.artist = song_split[0]
     song.artist_name(song_split[0])
-    song
   end
     
   
