4c4f786592ab703d37eb112dd5ecdabeb6ac5001
Jason Viger <jason.viger@gmail.com>
2018-08-20 07:34:41 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -32,7 +32,7 @@ class Artist
   def self.find_or_create_by_name(artist_name)
     if self.all.find {|artist| artist.name == artist_name }
       self.all.find {|artist| artist.name == artist_name }
-    else self.new(artist_name).map {|artist| artist.save}
+    else self.new(artist_name) {|artist| artist.save}
   end
 end
     
